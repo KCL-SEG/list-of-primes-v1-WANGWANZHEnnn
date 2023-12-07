2c0f19f4b80574a8b3847a30c8ba3549a25038a4
Wanzhen Wang <101357076+WANGWANZHEnnn@users.noreply.github.com>
2023-12-07 23:06:34 +0000

    Add files via upload

--- primes.py
+++ primes.py
@@ -6,6 +6,7 @@
 #    return list
 
 def is_prime(num):
+    """Check whether a number is prime."""
     if num <= 1:
         return False
     if num == 2:
@@ -18,11 +19,15 @@ def is_prime(num):
     return True
 
 def primes(number_of_primes):
+    """
+    Generate a list of the first 'number_of_primes' prime numbers.
+    If 'number_of_primes' is less than or equal to 0, return an empty list.
+    """
     if number_of_primes <= 0:
         return []
 
     prime_list = []
-    num = 2  # Start with the first prime number, 2
+    num = 2  # Starting with the first prime number
 
     while len(prime_list) < number_of_primes:
         if is_prime(num):
@@ -32,5 +37,7 @@ def primes(number_of_primes):
     return prime_list
 
 # Example usage:
-#result = primes(10)
+# result = primes(10)
+# print(result)  # Output: [2, 3, 5, 7, 11, 13, 17, 19, 23, 29]
+
 #print(result)  # Output: [2, 3, 5, 7, 11, 13, 17, 19, 23, 29]
